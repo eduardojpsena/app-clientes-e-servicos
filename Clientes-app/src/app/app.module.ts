fce49dfcdc53dc6805fd72d28228a87a3d58d90b
eduardojpsena <eduardojpsena@gmail.com>
2021-02-09 15:34:32 -0300

    concluindo template inicial

--- Clientes-app/src/app/app.module.ts
+++ Clientes-app/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { from } from 'rxjs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { TemplateModule } from './template/template.module'
+import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
